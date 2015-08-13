8d31b3ccd33080f42a4e6cd5a1c7879e91cd6150
Venj <ersaclarke@gmail.com>
2015-08-13 18:46:41 +0800

    Reorgnized playground structure.

---
+++ The Swift Programming Language.playground/Pages/第3章 基础知识.xcplaygroundpage/Contents.swift
@@ -0,0 +1,19 @@
+//: [<< 第2章Swift导览](@previous) | [Next >>](@next)
+
+/*:
+
+# 语言指南
+
+*/
+
+
+/*:
+
+## 基础知识
+
+Swift是开发iOS，OS X和watchOS应用的全新语言。尽管如此，Swift的很多方面对于有过C和Objective-C开发经验的程序员来说都会觉得很熟悉。
+
+*/
+
+
+//: [<< 第2章Swift导览](@previous) | [Next >>](@next)
